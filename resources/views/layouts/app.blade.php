4e0440ba821609e9358fd6475f4899a298d27f07
fedelucesoli <fedelucesoli@gmail.com>
2017-06-22 23:18:43 -0300

    Footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,8 +91,9 @@
         </nav> --}}
 
         @yield('content')
-    
 
+        @component('layouts.footer')
+        @endcomponent
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
